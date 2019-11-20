8020b1405e683faf6edb7204513f87cf0af7b1c8
Jose Manuel Aburto <jm.aburto01@gmail.com>
2019-11-20 13:29:28 +0100

    Update 2_Envipe_Prep.R

--- R/2_Envipe_Prep.R
+++ R/2_Envipe_Prep.R
@@ -1,5 +1,6 @@
 library(haven)
 library(foreign)
 
-Data.path <- 'C:/Users/jmaburto.SAM/OneDrive - Syddansk Universitet/INEGI Surveys'
+Data.path <- 'C:/Users/jmaburto.SAM/OneDrive - Syddansk Universitet/INEGI Surveys/ENSI/ensi_05/'
 
+read.dbf('C:/Users/jmaburto.SAM/OneDrive - Syddansk Universitet/INEGI Surveys/ENSI/ensi_05/Base de Datos/Bases/ENSIPOB.dbf',)
